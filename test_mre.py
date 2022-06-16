8455550a9e674f1884a61aa447ba6e4cb2cf6072
Blazej Dolicki <blazej.dol@gmail.com>
2022-06-16 13:32:30 +0200

    Add script with a simple test for the implementation of MRE

--- test_mre.py
+++ test_mre.py
@@ -1,5 +1,6 @@
 import argparse
 from argparse import Namespace
+import os
 
 from evaluate_mre import evaluate_mre
 import utils
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--dataset', type=str, help='Dataset used for evaluation')
     parser.add_argument('--data_dir', type=str, help='Directory of the data')
-    parser.add_argument('--split', type=str, required=True, choices=["train", "test", "valid"])
+    parser.add_argument('--split', type=str, choices=["train", "test", "valid"])
     parser.add_argument('--checkpoint_path', type=str, default=None,
                         help="Path to a trained model used for evaluation. "
                              "If not specified, use a randomly initialized model")
@@ -26,16 +27,19 @@ if __name__ == "__main__":
     parser.add_argument('--seed', type=int, help='Random seed')
     parser.add_argument("--log_dir", type=str, default="logs_eval",
                         help="Directory with logs: checkpoints, parameters, metrics")
-    parser.add_argument("--mlflow_dir", type=str, default="mlflow_runs",
+    parser.add_argument("--mlflow_dir", type=str, default=os.path.join(os.getcwd(),"mlflow_runs"),
                         help="Directory with MLFlow logs")
     parser.add_argument('--sample', type=float, default=None,
                         help='A fraction or number of examples that will be subsampled from the full dataset. '
                              'Useful for quick debugging and experiments with low data regimes.')
     parser.add_argument("--mre_n", type=int, default=4,
                         help='Number of rotations N sampled when evaluating MRE(N)')
+    parser.add_argument("--save_samples", type=int, default=0,
+                        help='Number of batches for which we plot the rotated images together with their probabilities.'
+                             'By default, no images are saved.')
 
     # split is not important for dummy dataset, but it's required argument
-    args = parser.parse_args(["--split", "test"])
+    args = parser.parse_args(namespace=Namespace(split="test"))
 
     args.dataset = "dummy"
 
