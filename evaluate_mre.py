d8c198b06170bbcc8dc12b13571990cfb4fb6170
Blazej Dolicki <blazej.dol@gmail.com>
2022-06-16 13:31:38 +0200

    Add option to save samples of rotated images with their predicted probabilities

--- evaluate_mre.py
+++ evaluate_mre.py
@@ -86,6 +86,8 @@ def evaluate_mre(args):
         assert transform_list_str == args.transform["test"], \
             "Current image transformations are different than those used for evaluation during training"
 
+
+
     utils.setup_mlflow(args)
 
     args_path = os.path.join(args.log_dir, "args.json")
@@ -105,6 +107,8 @@ def evaluate_mre(args):
         num_classes = 2
         dataset = DummyDataset(num_classes=num_classes)
 
+    logging.info(f"Train size {len(dataset)}")
+
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
     device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
@@ -112,7 +116,7 @@ def evaluate_mre(args):
     # select a model with randomly initialized weights, default is resnet18 so that we can train it quickly
     model = get_model(args.model_type, num_classes, args).to(device)
     if args.checkpoint_path is not None:
-        logging.debug(f"Loading model {args.model_type} from {args.checkpoint_path}")
+        logging.info(f"Loading model {args.model_type} from {args.checkpoint_path}")
         # load model from checkpoint
         state_dict = torch.load(args.checkpoint_path)
 
@@ -121,7 +125,10 @@ def evaluate_mre(args):
 
         model.load_state_dict(state_dict)
     else:
-        logging.debug(f"Model {args.model_type} initialized from scratch.")
+        logging.info(f"Model {args.model_type} initialized from scratch.")
+
+    rotated_imgs_path = os.path.join(args.log_dir, f'rotated_imgs')
+    os.makedirs(rotated_imgs_path, exist_ok=True)
 
     N = args.mre_n
     angles = np.linspace(start=0, stop=360, num=N, endpoint=False)
@@ -129,11 +136,17 @@ def evaluate_mre(args):
 
     stds = []
     with torch.no_grad():
-        for batch in dataloader:
+        for batch_idx, batch in enumerate(dataloader):
             model.eval()
 
             # shape of inputs: (B, C, H, W)
             inputs, labels = batch
+
+            # take first image
+            img = np.asarray(inputs[0].permute(1, 2, 0))
+            plt.imshow(img)
+            plt.imsave(f'image.png', img)
+
             # number of unique images in the batch
             batch_size = inputs.shape[0]
 
@@ -147,6 +160,7 @@ def evaluate_mre(args):
             # first B indices in the first dimension relate to all images in batch with the first rotation
             rotated_inputs = torch.cat(rotated_inputs, dim=0)
 
+
             rotated_inputs = rotated_inputs.to(device)
             labels = labels.to(device)
 
@@ -156,8 +170,28 @@ def evaluate_mre(args):
             # obtain probabilties from the outputs
             probs = F.softmax(outputs, dim=1)
 
+            if args.save_samples > batch_idx:
+                assert batch_size <= 10, "Batch size too large to plot all images."
+
+                # plot all images in batch and their rotations together with predicted probabilities
+                plt.rcParams.update({'font.size': 8})
+                columns = batch_size
+                rows = N
+                fig = plt.figure(figsize=(12, 12))
+                for i in range(0, columns * rows):
+                    img = np.asarray(rotated_inputs[i].permute(1, 2, 0).cpu())
+                    img_probs = [round(num, 4) for num in probs[i].tolist()]
+                    fig.add_subplot(rows, columns, i + 1, title=f"{img_probs}")
+                    plt.imshow(img)
+                fig.suptitle(f'Image labels {labels.tolist()}')
+                fig.tight_layout(rect=[0, 0.03, 1, 0.98])
+                samples_path = os.path.join(rotated_imgs_path, f'batch{batch_idx}.png')
+                plt.savefig(samples_path)
+
             #  reshape to (B, N, num_classes) in order to have separate dimensions for images and rotations
             probs = probs.reshape((N, batch_size, -1)).permute(1, 0, 2)
+
+
 
             # reshape labels to select the correct class probability in gather()
             label_indices = labels.unsqueeze(dim=1).unsqueeze(dim=2).repeat(1, N, 1)
@@ -167,10 +201,9 @@ def evaluate_mre(args):
 
             # compute standard deviations between rotations of each image and add it to the list
             batch_stds = torch.std(target_probs, dim=1).squeeze()
-            # print("batch probs", probs)
-            # print("batch stds", batch_stds.tolist())
+
             stds += batch_stds.tolist()
-    # print("stds", stds)
+
     mre = np.mean(stds)
 
     logging.info(f"MRE({N}): \t{mre}")
@@ -209,6 +242,9 @@ if __name__ == "__main__":
                              'Useful for quick debugging and experiments with low data regimes.')
     parser.add_argument("--mre_n", type=int, default=4,
                         help='Number of rotations N sampled when evaluating MRE(N)')
+    parser.add_argument("--save_samples", type=int, default=0,
+                        help='Number of batches for which we plot the rotated images together with their probabilities.'
+                             'By default, no images are saved.')
     args = parser.parse_args()
 
     mre = evaluate_mre(args)
