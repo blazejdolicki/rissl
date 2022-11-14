55c8bb4d076a25eb4f5531e78802fe8df2953569
Blazej Dolicki <blazej.dol@gmail.com>
2022-11-14 13:59:01 +0100

    Add example of loading JIT model

---
+++ tools/test_jit_model.py
@@ -0,0 +1,19 @@
+import torch
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", type=int, default=512)
+parser.add_argument("--img_width", type=int, default=224)
+parser.add_argument("--img_height", type=int, default=224)
+parser.add_argument("--img_channels", type=int, default=3)
+parser.add_argument("--jit_checkpoint_path", type=str, required=True)
+args = parser.parse_args()
+
+example = torch.rand(args.batch_size, args.img_channels, args.img_height, args.img_width)
+jit_model = torch.jit.load(args.jit_checkpoint_path)
+output = jit_model(example)
+
+print("Output shape", output.shape)
+print("Output")
+print(output)
+
